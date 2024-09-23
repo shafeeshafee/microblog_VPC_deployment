465ffc35e431a09f823b11c9ef2bec752d12bba2
shafeeshafee <shafeelinks@gmail.com>
2024-09-23 02:00:58 -0400

    include cloning mechanism on web server as well

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,9 +9,19 @@ fi
 # Variables
 APP_SERVER_IP="$1"
 APP_SERVER_KEY="/home/ubuntu/.ssh/app_server_key.pem"
+REPO_URL="https://github.com/shafeeshafee/microblog_VPC_deployment.git"
+REPO_DIR="/home/ubuntu/microblog_VPC_deployment"
+
+# Clone the repository if it doesn't exist
+if [ ! -d "$REPO_DIR" ]; then
+    git clone "$REPO_URL" "$REPO_DIR"
+else
+    cd "$REPO_DIR"
+    git pull origin main
+fi
 
 # Copy start_app.sh to the Application Server
-scp -i "$APP_SERVER_KEY" ~/microblog_VPC_deployment/scripts/start_app.sh ubuntu@"$APP_SERVER_IP":/home/ubuntu/
+scp -i "$APP_SERVER_KEY" "$REPO_DIR/scripts/start_app.sh" ubuntu@"$APP_SERVER_IP":/home/ubuntu/
 
 # Run start_app.sh on the Application Server
 ssh -i "$APP_SERVER_KEY" ubuntu@"$APP_SERVER_IP" 'bash ~/start_app.sh'
