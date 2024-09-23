5e2adfb5d6fb843765795a0f596170f38ce385d5
shafeeshafee <shafeelinks@gmail.com>
2024-09-23 00:15:15 -0400

    add logic for setup and startup scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <APP_SERVER_IP>"
+    exit 1
+fi
+
+# Variables
+APP_SERVER_IP="$1"
+APP_SERVER_KEY="/home/ubuntu/.ssh/app_server_key.pem"
+
+# Copy start_app.sh to the Application Server
+scp -i "$APP_SERVER_KEY" ~/microblog_VPC_deployment/scripts/start_app.sh ubuntu@"$APP_SERVER_IP":/home/ubuntu/
+
+# Run start_app.sh on the Application Server
+ssh -i "$APP_SERVER_KEY" ubuntu@"$APP_SERVER_IP" 'bash ~/start_app.sh'
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+
+# update and install deps
+sudo apt update
+sudo apt install -y python3-pip python3-venv git
+
+# clone the repo, else update if need
+if [ ! -d "/home/ubuntu/microblog_VPC_deployment" ]; then
+    git clone https://github.com/shafeeshafee/microblog_VPC_deployment.git /home/ubuntu/microblog_VPC_deployment
+else
+    cd /home/ubuntu/microblog_VPC_deployment
+    git pull origin main
+fi
+
+cd /home/ubuntu/microblog_VPC_deployment
+
+# sets up venv
+python3 -m venv venv
+source venv/bin/activate
+
+# install requirements
+pip install --upgrade pip
+pip install -r requirements.txt
+pip install gunicorn pymysql cryptography
+
+# set environment variables
+export FLASK_APP=microblog.py
+export FLASK_ENV=production
+
+# database migrations
+flask db upgrade
+
+# starts the application using Gunicorn via systemd
+sudo systemctl restart microblog
